3952ca3682a6786bd5ed9c6cee596b87f2ab81ea
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-09-16 19:04:11 -0500

    Switch to commonjs

--- wwwroot/systemjs.config.js
+++ wwwroot/systemjs.config.js
@@ -11,12 +11,15 @@
         },
         // map tells the System loader where to look for things
         map: {
+            app: 'dist', // app is within dist folder when it gets compiled
             'angular':        'npm:angular/angular.js',
             'angular-route':  'npm:angular-route/angular-route.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
-
+            app: {
+                main: './main'
+              }
         }
     });
 })(this);
