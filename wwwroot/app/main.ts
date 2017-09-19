c60fade4d995198020e088aad62947e7f73de759
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-09-19 10:53:51 -0500

    Pulling in Bootstrap
    Start out a simple menu and show a view

--- wwwroot/app/main.ts
+++ wwwroot/app/main.ts
@@ -6,6 +6,11 @@ import { configES6Promise } from "./configES6Promise";
 import { routeConfig} from "./routeConfig";
 import {GeneralService} from "./services";
 
+// import jquery, jquery-ui, and bootstrap so they get loaded
+import * as jquery from "jquery";
+import "jquery-ui";
+import "bootstrap";
+
 
 const angularAppName = "angularJsDemoApp";
 const app = angular.module(angularAppName, ['ngRoute']);
--- wwwroot/systemjs.config.js
+++ wwwroot/systemjs.config.js
@@ -13,7 +13,10 @@
         map: {
             app: 'dist', // app is within dist folder when it gets compiled
             'angular':        'npm:angular/angular.js',
-            'angular-route':  'npm:angular-route/angular-route.js'
+            'angular-route':  'npm:angular-route/angular-route.js',
+            "jquery": "npm:jquery/dist/jquery.min.js",
+            "jquery-ui": "npm:jquery-ui-dist/jquery-ui.min.js",
+            "bootstrap":"npm:bootstrap/dist/js/bootstrap.min.js"
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -24,6 +27,10 @@
             "angular":{
                 "format": "global",
                 "exports": "angular"
+            },
+            "jquery":{
+                "format":"global",
+                "exports":"jQuery"
             }
         }
     });
